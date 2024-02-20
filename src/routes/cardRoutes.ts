4cc1988ba6bf5c05497742afcfb8aa6658e062a3
Rose2999 <153045249+Rose2999@users.noreply.github.com>
2024-02-20 22:18:17 +0530

    feat:added delete card

--- src/routes/cardRoutes.ts
+++ src/routes/cardRoutes.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from 'express';
 import getContactListService from '../services/cardServices/getContactListService';
 import createNewCardService from '../services/cardServices/createNewCardService';
-import getCardListService from '../services/cardServices/getCardListService';
 import addToExistingContactService from '../services/cardServices/addToExistingContactService';
-
+import getCardListController from '../controllers/cardControllers/getCardListController';
+import deleteCardController from '../controllers/cardControllers/deleteCardController';
 const router = Router();
 //API end point to get contact list in homepage
 router.get('/getContactList', async (req: Request, res: Response) => {
@@ -15,11 +15,14 @@ router.post('/createNewCard', async (req: Request, res: Response) => {
 });
 
 router.get('/getCardList', async (req: Request, res: Response) => {
-  getCardListService(req, res);
+  getCardListController(req, res);
 });
 
 router.post('/addToExistingContact', async (req: Request, res: Response) => {
   addToExistingContactService(req, res);
 });
+router.patch('/deleteCard',async(req:Request,res:Response)=>{
+    deleteCardController(req,res);
+})
 
 export default router;
