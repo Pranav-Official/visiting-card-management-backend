01b404525cff38e5d6d034ff77d373c74d693764
Vinay Thrivikraman <vinay.thrivikraman@experionglobal.local>
2024-02-20 18:05:25 +0530

    feat: added the initial controller fix

---
+++ src/controllers/cardControllers/addToExistingCardController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import addToExistingContactService from "../../services/cardServices/addToExistingContactService";
+
+const addToExistingContactController = (req: Request, res: Response) => {
+    
+}
+
+export default addToExistingContactController;
--- src/routes/cardRoutes.ts
+++ src/routes/cardRoutes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import getContactListService from '../services/cardServices/getContactListService';
 import createNewCardService from '../services/cardServices/createNewCardService';
 import getCardListService from '../services/cardServices/getCardListService';
-import addToExistingContactService from '../services/cardServices/addToExistingContactService';
+import addToExistingContactController from '../controllers/cardControllers/addToExistingCardController';
 
 const router = Router();
 //API end point to get contact list in homepage
@@ -19,7 +19,7 @@ router.get('/getCardList', async (req: Request, res: Response) => {
 });
 
 router.post('/addToExistingContact', async (req: Request, res: Response) => {
-  addToExistingContactService(req, res);
+  addToExistingContactController(req, res);
 });
 
 export default router;
