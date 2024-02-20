402c971967e0e025c8842baf3f9f42d187b2b16f
Anly S <anly.s@experionglobal.local>
2024-02-20 20:31:18 +0530

    fix:added changes in getProfile

--- src/controllers/profileControllers/getProfileController.ts
+++ src/controllers/profileControllers/getProfileController.ts
@@ -1,18 +1,22 @@
-//controller for profile data fetch
 import { Request, Response } from 'express';
 import getProfileService from '../../services/profile/getProfileService';
 
+// Controller for fetching profile data
 const getProfileController = async (req: Request, res: Response) => {
   try {
-    //finding user
+    // Extracting user_id from the request body
     const { user_id } = req.body;
+
     if (!user_id) {
-      return res.status(404).json({
-        message: 'User not found!',
+      return res.status(400).json({
+        message: 'User ID not provided',
       });
     }
-    //passing user id to getProfileService function
+
+    // Calling the service function to retrieve profile details
     const profileDetails = await getProfileService(user_id);
+
+    // Returning profile details in the response
     return res.status(200).json(profileDetails);
   } catch (error) {
     console.error('Error:', error);
--- src/services/profile/getProfileService.ts
+++ src/services/profile/getProfileService.ts
@@ -2,38 +2,45 @@ import UserTable from '../../models/userTable';
 import Cards from '../../models/cards';
 import SharedCards from '../../models/sharedCards';
 
-//getProfileService function to fetch profile details
+// Function to fetch profile details
 const getProfileService = async (user_id: string) => {
   try {
+    // Finding user details
     const user = await UserTable.findOne({
       where: {
         user_id: user_id,
       },
       raw: true,
     });
+
+    // Logging user details
     console.log(user);
 
-    //totalContacts count
+    // Counting total contacts
     const totalContacts = await Cards.count({
       where: {
         user_id: user_id,
         parent_card_id: null,
       },
     });
-    //total accepted cards count
+
+    // Counting total accepted cards
     const acceptedCards = await Cards.count({
       where: {
         user_id: user_id,
         shared_or_not: 1,
       },
     });
-    //total pending cards count
+
+    // Counting total pending cards
     const pendingCards = await SharedCards.count({
       where: {
         user_id: user_id,
         status: 'pending',
       },
     });
+
+    // Returning profile details
     return {
       fullName: user.user_fullname,
       email: user.user_email,
