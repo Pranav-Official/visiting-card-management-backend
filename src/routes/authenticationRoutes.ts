886b66f42e78f1da0cd3ae338d5d1723746dbaf6
Pranav-Official <pranavhtjp07@gmail.com>
2024-02-22 23:32:32 +0530

    feat:added login and jwt verification

--- src/routes/authenticationRoutes.ts
+++ src/routes/authenticationRoutes.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from 'express';
 import userRegistrationController from '../controllers/authentication/userRegistrationController';
+import userLoginController from '../controllers/authentication/userLoginController';
 
 const router = Router();
 
 router.post('/userRegistration', async (req: Request, res: Response) => {
   userRegistrationController(req, res);
+});
+
+router.get('/userLogin', async (req: Request, res: Response) => {
+  userLoginController(req, res);
 });
 
 export default router;
