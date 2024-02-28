afc5ee76fc8ae4c1497634b2d5cb133c76f05a92
Devapriya L <devapriyakodinjoor24@gmail.com>
2024-02-28 14:23:17 +0530

    first edit

--- src/services/cardServices/acceptCardService.ts
+++ src/services/cardServices/acceptCardService.ts
@@ -1,8 +1,7 @@
 //To accept the shared cards
-
-import Cards from '../../models/cards';
-import SharedCards from '../../models/sharedCards';
 import createNewCardService from './createNewCardService';
+import getCardDetailsService from './getCardDetailsService';
+import sharedCardAcceptService from './sharedCardAcceptService';
 
 interface AcceptCardParams {
   card_id: string;
@@ -13,43 +12,36 @@ const acceptCardService = async ({ card_id, user_id }: AcceptCardParams) => {
   try {
     const oldCardId = card_id;
     const receiverId = user_id;
-    const CardDetails = await Cards.findOne({
-      where: { card_id: oldCardId },
-      attributes: { exclude: ['user_id'] },
-      raw: true,
-    });
+
+    let CardDetails = await getCardDetailsService(oldCardId);
+    console.log('card details:',CardDetails);
 
     if (!CardDetails) {
       console.error('Error retrieving card details from Cards table');
       return { error: 'Error retrieving card details from Cards table' };
     }
+    // Add user_id to CardDetails
+    CardDetails.data = {
+      ...CardDetails,
+      user_id: receiverId,
+    };
+    // if(CardDetails) {
+    //  CardDetails.data.update({user_id:receiverId});
+    // }
 
-    CardDetails.user_id = receiverId;
+   // CardDetails.user_id = receiverId;
     console.log('cardData;', CardDetails);
-    console.log('user_id:', CardDetails.user_id);
+   // console.log('user_id:', CardDetails.user_id);
     console.log('New cardData;', CardDetails);
-    const success = await createNewCardService(CardDetails);
+    
+    const success = await createNewCardService(CardDetails.data);
+    // card_id of the newly created card
+    const cardId = success.card_id;
 
-    if (success.success) {
-      const cardId = success.card_id;
-      console.log('cardId:', cardId);
-      const newStatus = 'accepted';
+    const updatedCard = await sharedCardAcceptService(oldCardId, cardId, receiverId);
 
-      //Change the shared_or_not status in cards table
-      await Cards.update({ shared_or_not: 1 }, { where: { card_id: cardId } });
-
-      //Update the pending status in sharedCard table
-      const updatedCard = await SharedCards.update(
-        { status: newStatus, modifiedAt: new Date() },
-        { where: { card_id: oldCardId } },
-      );
-      console.log(updatedCard);
-
-      return updatedCard;
-    } else {
-      console.error('Error retrieving user_id from SharedCards');
-      return { error: 'Error retrieving user_id from SharedCards' };
-    }
+    console.log(updatedCard);
+    return updatedCard;
   } catch (error) {
     console.error('Error accepting card:', error);
     return { error: error.message };
