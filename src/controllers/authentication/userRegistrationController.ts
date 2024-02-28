8dba4f1e46e6ab455074aa0c9c7f5147db6a1e0d
Vinay Thrivikraman <vinay.thrivikraman@experionglobal.local>
2024-02-28 12:45:38 +0530

    fix: Fixed the userRegistration Return details

--- src/controllers/authentication/userRegistrationController.ts
+++ src/controllers/authentication/userRegistrationController.ts
@@ -2,33 +2,44 @@ import { Request, Response } from 'express';
 import userRegistrationService from '../../services/authentication/userRegistrationService';
 import { validateEmail } from '../../utils/regexChecks';
 
-const userResgistrationController = async (req: Request, res: Response) => {
+const userResgistrationController = async (
+  req: Request,
+  res: Response<responseType>,
+) => {
   const { user_fullname, user_email, password } = req.body;
 
   if (!user_fullname || !user_email || !password) {
-    return res.status(402).json({ error: 'Please Enter All the Details' });
+    return res.status(400).json({
+      status: false,
+      message: 'Please Enter All the Details',
+      data: {},
+    });
   }
 
   const emailValidity = validateEmail(user_email);
   if (!emailValidity) {
-    return res.status(402).json({ error: 'Please Enter a Valid Email' });
+    return res.status(400).json({
+      status: false,
+      message: 'Please Enter a Valid Email',
+      data: {},
+    });
   }
 
   try {
     //calls the userRegistrationService
-    const returnedValue = await userRegistrationService(
+    const userRegistrationStatus = await userRegistrationService(
       user_fullname,
       user_email,
       password,
     );
 
-    if (returnedValue.status == true) {
-      return res.status(200).json(returnedValue);
+    if (userRegistrationStatus.status === true) {
+      return res.status(200).json(userRegistrationStatus);
     } else {
-      return res.status(402).json(returnedValue);
+      return res.status(400).json(userRegistrationStatus);
     }
   } catch (error) {
-    return res.status(402).json({
+    return res.status(400).json({
       status: false,
       message: 'user registration failed' + error,
       user_id: null,
