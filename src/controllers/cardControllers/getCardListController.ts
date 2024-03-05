96d0be559d09c069848e64c0247211b16bb32ad1
Vijaysaintgits <vijayr2906@gmail.com>
2024-03-05 21:38:06 +0530

    fix:StatusCodes fix

--- src/controllers/cardControllers/getCardListController.ts
+++ src/controllers/cardControllers/getCardListController.ts
@@ -1,4 +1,5 @@
 
+import { StatusCodes } from "http-status-codes";
 import getCardListService from "../../services/cardServices/getCardListService";
 import { Request, Response } from "express";
  
@@ -9,14 +10,14 @@ const getCardListController = async(req: Request, res: Response) => {
 
           const getCardList=await  getCardListService(card_id);
           if(getCardList.status){
-          return res.status(200).send(getCardList.message);
+          return res.status(StatusCodes.OK).send(getCardList.message);
           }
           else{
-            return res.status(404).json(getCardList.message);
+            return res.status(StatusCodes.BAD_REQUEST).json(getCardList.message);
           }}
     catch(error){
         console.error('error',error);
-        return res.status(500).json({ error: 'Internal server error' });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal server error' });
     }
 };
  
