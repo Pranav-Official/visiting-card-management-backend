e16a19b5db45e717f6afcd822c6051c6038e611c
Vinay Thrivikraman <vinay.thrivikraman@experionglobal.local>
2024-03-06 12:04:52 +0530

    fix: fixed the issue caused during merge

--- src/controllers/cardControllers/getCardListController.ts
+++ src/controllers/cardControllers/getCardListController.ts
@@ -1,25 +1,16 @@
 import getCardListService from '../../services/cardServices/getCardListService';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { StatusCodes } from "http-status-codes";
-import getCardListService from "../../services/cardServices/getCardListService";
-import { Request, Response } from "express";
- 
-const getCardListController = async(req: Request, res: Response) => {
-    try{
 
-        const card_id = req.query.card_id as string;
+const getCardListController = async (req: Request, res: Response) => {
+  try {
+    const card_id = req.query.card_id as string;
 
-          const getCardList=await  getCardListService(card_id);
-          if(getCardList.status){
-          return res.status(StatusCodes.OK).send(getCardList.message);
-          }
-          else{
-            return res.status(StatusCodes.BAD_REQUEST).json(getCardList.message);
-          }}
-    catch(error){
-        console.error('error',error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal server error' });
+    const getCardList = await getCardListService(card_id);
+    if (getCardList.status) {
+      return res.status(StatusCodes.OK).send(getCardList.message);
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).json(getCardList.message);
     }
   } catch (error) {
     console.error('error', error);
