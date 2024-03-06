74fdaa6b921b30aa98fcf976f37f021e62e84380
Vinay Thrivikraman <vinay.thrivikraman@experionglobal.local>
2024-03-06 12:03:52 +0530

    fix: fixed returns and integration of user registration and add to existing contacts

--- src/services/cardServices/addToExistingContactService.ts
+++ src/services/cardServices/addToExistingContactService.ts
@@ -1,22 +1,13 @@
 import Cards from '../../models/cards';
+import createNewCardService from './createNewCardService';
 
 //function to add card to existing contact
 const addToExistingContactService = async (
-  parent_card_id,
-  card_name,
-  img_front_link,
-  img_back_link,
-  job_title,
-  email,
-  phone,
-  company_name,
-  company_website,
-  description,
-  user_id,
-  shared_or_not,
+  parent_card_id: string,
+  cardData: Cards,
 ) => {
   try {
-    //to check if the parent card exists in the database
+    //To check if the parent card exists in the database
     const findParentCard = await Cards.findOne({
       where: { card_id: parent_card_id },
       raw: true,
@@ -24,31 +15,38 @@ const addToExistingContactService = async (
 
     //to add card to and existing contact if parent card is found
     if (findParentCard != null) {
-      const createdCard = await Cards.create({
-        card_name,
-        img_front_link,
-        img_back_link,
-        job_title,
-        email,
-        phone,
-        company_name,
-        company_website,
-        description,
-        parent_card_id: parent_card_id,
-        user_id,
-        shared_or_not,
-      });
+      cardData.contact_name = '';
+      //uses the createNewCardService to create a new card with details
+      const createdCard = await createNewCardService(cardData);
 
-      if (createdCard) {
-        return { message: 'Card Added Successfully!', status: true };
+      if (createdCard.status === true) {
+        //sets the parent_card_id of the newly created card
+        const setParentCard = await Cards.update(
+          { parent_card_id: parent_card_id },
+          { where: { card_id: createdCard.data.cardId } },
+        );
+
+        if (setParentCard[0] === 1) {
+          return {
+            status: true,
+            message: 'Card Added Successfully!',
+            data: createdCard.data,
+          };
+        } else {
+          return {
+            status: false,
+            message: 'Couldnt Update Parent Card ID',
+            data: {},
+          };
+        }
       } else {
-        return { error: 'Failed To Add Card', status: false };
+        return { status: false, message: 'Unable To Add Card', data: {} };
       }
     } else {
-      return { error: 'Parent Card Not Found!', status: false };
+      return { status: false, message: 'Parent Card Not Found', data: {} };
     }
   } catch (error) {
-    return { error: error, status: false };
+    return { status: false, message: 'Failed To Add Card', data: {} };
   }
 };
 
