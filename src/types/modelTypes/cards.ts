d8644676bee9c636c6ef5e84c9e7d9d291fdcbd2
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-03-15 12:07:45 +0530

    remove warnings due to using public overshadowing getters in sequelize.

--- src/types/modelTypes/cards.ts
+++ src/types/modelTypes/cards.ts
@@ -1,39 +1,21 @@
 import { Model } from 'sequelize';
-interface CardType {
-  card_id: string;
-  card_name: string | null;
-  img_front_link: string | null;
-  img_back_link: string | null;
-  job_title: string | null;
-  email: string | null;
-  phone: string | null;
-  company_name: string | null;
-  company_website: string | null;
-  description: string | null;
-  contact_name: string | null;
-  parent_card_id: string | null;
-  user_id: string | null;
-  shared_or_not: number | null;
-  createdAt: Date | null;
-  modifiedAt: Date | null;
-}
-class Cards extends Model<CardType> {
-  card_id: string;
-  card_name: string | null;
-  img_front_link: string | null;
-  img_back_link: string | null;
-  job_title: string | null;
-  email: string | null;
-  phone: string | null;
-  company_name: string | null;
-  company_website: string | null;
-  description: string | null;
-  contact_name: string | null;
-  parent_card_id: string | null;
-  user_id: string | null;
-  shared_or_not: number | null;
-  createdAt: Date | null;
-  modifiedAt: Date | null;
+class Cards extends Model {
+  declare card_id: string;
+  declare card_name: string | null;
+  declare img_front_link: string | null;
+  declare img_back_link: string | null;
+  declare job_title: string | null;
+  declare email: string | null;
+  declare phone: string | null;
+  declare company_name: string | null;
+  declare company_website: string | null;
+  declare description: string | null;
+  declare contact_name: string | null;
+  declare parent_card_id: string | null;
+  declare user_id: string | null;
+  declare shared_or_not: number | null;
+  declare createdAt: Date | null;
+  declare modifiedAt: Date | null;
 }
 
 export default Cards;
