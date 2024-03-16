22bd7f6af0ab3c056206a16fe1136479bcc4535a
Devapriya L <devapriyakodinjoor24@gmail.com>
2024-03-16 11:42:48 +0530

    feat: accept card test

---
+++ __tests__/controllers/acceptCard.test.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+import * as acceptCardsModule from '../../src/services/cardServices/acceptCardService';
+import acceptCardsController from '../../src/controllers/cardControllers/acceptCard';
+
+const jsonSpy = sinon.spy();
+
+const res: Partial<Response> = {
+  status: sinon.stub().returnsThis(),
+  json: jsonSpy,
+};
+
+describe('acceptCards Controller', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Accepts cards successfully', async () => {
+    const req: Partial<Request> = {
+      body: {
+        card_id: '1626aeea-6355-40f8-a141-5a58d2dd420b',
+        user_id: '891f8cd0-c369-452e-b514-583d86cb0d0c',
+        contact_name: 'Jane Doe',
+      },
+    };
+
+    const acceptCardsStub = sinon.stub(acceptCardsModule, 'default').resolves({
+      status: true,
+      message: 'Shared card accepted successfully',
+      data: [],
+    });
+
+    await acceptCardsController(req as Request, res as Response);
+
+    sinon.assert.called(acceptCardsStub);
+    sinon.assert.calledWith(jsonSpy, {
+      status: true,
+      message: 'Shared card accepted successfully',
+      data: [],
+    });
+  });
+
+  it('Does not accept cards', async () => {
+    const req: Partial<Request> = {
+      body: {
+        card_id: '1626aeea-6355-40f8-a141-5a58d2dd420b',
+        user_id: '891f8cd0-c369-452e-b514-583d86cb0d0c',
+        contact_name: 'Jane Doe',
+      },
+    };
+
+    const acceptCardsStub = sinon.stub(acceptCardsModule, 'default').resolves({
+      status: false,
+      message: 'Error in accepting shared card',
+      data: {},
+    });
+
+    await acceptCardsController(req as Request, res as Response);
+
+    sinon.assert.called(acceptCardsStub);
+    sinon.assert.calledWith(jsonSpy, {
+      status: false,
+      message: 'Error in accepting shared card',
+      data: {},
+    });
+  });
+});
