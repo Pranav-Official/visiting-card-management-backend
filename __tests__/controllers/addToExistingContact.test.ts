402548c882c237a94d9617aa78971ad807f10754
Devapriya L <devapriyakodinjoor24@gmail.com>
2024-03-18 18:18:28 +0530

    feat: uncommented addToExistingContact test

--- __tests__/controllers/addToExistingContact.test.ts
+++ __tests__/controllers/addToExistingContact.test.ts
@@ -1,78 +1,78 @@
-// import { Request, Response } from 'express';
-// import * as sinon from 'sinon';
-// import * as addToExistingContactServiceModule from '../../src/services/cardServices/addToExistingContactService';
-// import addToExistingContactController from '../../src/controllers/cardControllers/addToExistingCardController';
-// const req: Partial<Request> = {
-//   body: {},
-// };
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+import * as addToExistingContactServiceModule from '../../src/services/cardServices/addToExistingContactService';
+import addToExistingContactController from '../../src/controllers/cardControllers/addToExistingCardController';
+const req: Partial<Request> = {
+  body: {},
+};
 
-// const jsonSpy = sinon.spy();
-// const res: Partial<Response> = {
-//   status: sinon.stub().returnsThis(),
-//   json: jsonSpy,
-// };
-// describe('add to existing card controller', () => {
-//   afterEach(() => {
-//     sinon.restore();
-//   });
+const jsonSpy = sinon.spy();
+const res: Partial<Response> = {
+  status: sinon.stub().returnsThis(),
+  json: jsonSpy,
+};
+describe('add to existing card controller', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
 
-//   it('parent card id not found error', async () => {
-//     const addToExtstingContactSpy = sinon.spy(addToExistingContactController);
-//     const addToExistingContactServiceStub = sinon.stub().resolves({
-//       status: false,
-//       message: 'Parent Card Id Not Received',
-//       data: {},
-//     });
-//     sinon.replace(
-//       addToExistingContactServiceModule,
-//       'default',
-//       addToExistingContactServiceStub,
-//     );
-//     await addToExtstingContactSpy(req as Request, res as Response);
-//     sinon.assert.called(addToExtstingContactSpy);
-//     sinon.assert.calledWith(jsonSpy, {
-//       status: false,
-//       message: 'Parent Card Id Not Received',
-//       data: {},
-//     });
-//   });
+  it('parent card id not found error', async () => {
+    const addToExtstingContactSpy = sinon.spy(addToExistingContactController);
+    const addToExistingContactServiceStub = sinon.stub().resolves({
+      status: false,
+      message: 'Parent Card Id Not Received',
+      data: {},
+    });
+    sinon.replace(
+      addToExistingContactServiceModule,
+      'default',
+      addToExistingContactServiceStub,
+    );
+    await addToExtstingContactSpy(req as Request, res as Response);
+    sinon.assert.called(addToExtstingContactSpy);
+    sinon.assert.calledWith(jsonSpy, {
+      status: false,
+      message: 'Parent Card Id Not Received',
+      data: {},
+    });
+  });
 
-//   it('missing required fields error', async () => {
-//     const addToExtstingContactSpy = sinon.spy(addToExistingContactController);
-//     const createNewCardServiceStub = sinon
-//       .stub()
-//       .resolves({ status: false, message: 'Card creation failed', data: {} });
-//     sinon.replace(
-//       addToExistingContactServiceModule,
-//       'default',
-//       createNewCardServiceStub,
-//     );
-//     await addToExtstingContactSpy(req as Request, res as Response);
-//     sinon.assert.called(addToExtstingContactSpy);
-//     sinon.assert.calledWith(jsonSpy, {
-//       status: false,
-//       message: 'Card creation failed',
-//       data: {},
-//     });
-//   });
+  it('missing required fields error', async () => {
+    const addToExtstingContactSpy = sinon.spy(addToExistingContactController);
+    const createNewCardServiceStub = sinon
+      .stub()
+      .resolves({ status: false, message: 'Card creation failed', data: {} });
+    sinon.replace(
+      addToExistingContactServiceModule,
+      'default',
+      createNewCardServiceStub,
+    );
+    await addToExtstingContactSpy(req as Request, res as Response);
+    sinon.assert.called(addToExtstingContactSpy);
+    sinon.assert.calledWith(jsonSpy, {
+      status: false,
+      message: 'Card creation failed',
+      data: {},
+    });
+  });
 
-//   it('Successful card creation', async () => {
-//     const responseMock = {
-//       status: true,
-//       message: 'Successfull',
-//       data: {
-//         cardId: '',
-//       },
-//     };
-//     const createNewCardSpy = sinon.spy(createNewCardController);
-//     const createNewCardServiceStub = sinon.stub().resolves(responseMock);
-//     sinon.replace(
-//       createNewCardServiceModule,
-//       'default',
-//       createNewCardServiceStub,
-//     );
-//     await createNewCardSpy(req as Request, res as Response);
-//     sinon.assert.called(createNewCardSpy);
-//     sinon.assert.calledWith(jsonSpy, responseMock);
-//   });
-// });
+  it('Successful card creation', async () => {
+    const responseMock = {
+      status: true,
+      message: 'Successfull',
+      data: {
+        cardId: '',
+      },
+    };
+    const createNewCardSpy = sinon.spy(createNewCardController);
+    const createNewCardServiceStub = sinon.stub().resolves(responseMock);
+    sinon.replace(
+      createNewCardServiceModule,
+      'default',
+      createNewCardServiceStub,
+    );
+    await createNewCardSpy(req as Request, res as Response);
+    sinon.assert.called(createNewCardSpy);
+    sinon.assert.calledWith(jsonSpy, responseMock);
+  });
+});
