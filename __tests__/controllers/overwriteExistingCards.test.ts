aab4c7dd9aa5b367d2044a794f0c3a7deb1e1b6e
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-03-19 16:15:34 +0530

    add two more test on overwriteExistingCard endpoint

--- __tests__/controllers/overwriteExistingCards.test.ts
+++ __tests__/controllers/overwriteExistingCards.test.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import * as sinon from 'sinon';
 import overWriteCardController from '../../src/controllers/cardControllers/overWriteExistingCard';
-
+import * as getCardDetailsServiceModule from '../../src/services/cardServices/getCardDetailsService';
 const jsonSpy = sinon.spy();
 const res: Partial<Response> = {
   status: sinon.stub().returnsThis(),
   json: jsonSpy,
 };
-describe('edit card controller', () => {
+describe('overwriteExistingCard controller', () => {
   afterEach(() => {
     sinon.restore();
   });
@@ -16,13 +16,54 @@ describe('edit card controller', () => {
   };
 
   it('missing required fields error', async () => {
-    const editCardSpy = sinon.spy(overWriteCardController);
-    await editCardSpy(req as Request, res as Response);
-    sinon.assert.called(editCardSpy);
+    const overWriteCardSpy = sinon.spy(overWriteCardController);
+    await overWriteCardSpy(req as Request, res as Response);
+    sinon.assert.called(overWriteCardSpy);
     sinon.assert.calledWith(jsonSpy, {
       status: false,
       message: 'Not all necessary parameters passed',
       data: {},
+    });
+  });
+
+  it('wrong card id passed', async () => {
+    const req: Partial<Request> = {
+      body: { shared_card_id: 'shared_id', card_to_overWrite: 'card_id' },
+    };
+    const overWriteCardSpy = sinon.spy(overWriteCardController);
+    const getCardDetailsServiceStub = sinon.stub().resolves({ status: false });
+    sinon.replace(
+      getCardDetailsServiceModule,
+      'default',
+      getCardDetailsServiceStub,
+    );
+    await overWriteCardSpy(req as Request, res as Response);
+    sinon.assert.called(overWriteCardSpy);
+    sinon.assert.calledWith(jsonSpy, {
+      status: false,
+      message: 'Card You Shared couldnt be found',
+      data: {},
+    });
+  });
+
+  it('Internal server Error on DB error', async () => {
+    const req: Partial<Request> = {
+      body: { shared_card_id: 'shared_id', card_to_overWrite: 'card_id' },
+    };
+    const overWriteCardSpy = sinon.spy(overWriteCardController);
+    const getCardDetailsServiceStub = sinon
+      .stub()
+      .rejects(new Error('Test error'));
+    sinon.replace(
+      getCardDetailsServiceModule,
+      'default',
+      getCardDetailsServiceStub,
+    );
+    await overWriteCardSpy(req as Request, res as Response);
+    sinon.assert.called(overWriteCardSpy);
+    sinon.assert.calledWithMatch(jsonSpy, {
+      status: false,
+      message: 'Internal Server Error',
     });
   });
 });
